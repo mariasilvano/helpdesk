9ae04de6b794c7edbe3d30a02184559a8d9628f7
Maria Silvano <mariagabrielasilvano@gmail.com>
2023-08-11 19:18:44 -0300

    Inicio do projeto, tela de login e do usuario

---
+++ public_html/user.js
@@ -0,0 +1,13 @@
+function toogleMenu(){
+    const menuMobile = document.getElementById("menu-mobile")
+
+    if(menuMobile.className === "menu-mobile-active"){
+        menuMobile.className = "menu-mobile"
+    }else{
+        menuMobile.className = "menu-mobile-active"
+    }
+}
+
+function novoTicket(){
+/*FAZER FUÇÃO QUE SUBSTITUA O H1 DA MAIN POR UM FORM QUE SERÁ USADO PARA CADASTRAR UM NOVO TICKET*/ 
+}
